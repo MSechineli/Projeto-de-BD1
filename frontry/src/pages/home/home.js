b7c13e3ce54d6d73f8b9e424a754226d9a9af5df
MSechineli <matheussechineli@gmail.com>
2019-12-10 23:15:21 -0300

    front melhorias porém ainda não funcional

--- frontry/src/pages/home/home.js
+++ frontry/src/pages/home/home.js
@@ -1,23 +1,60 @@
-import React, { Component } from 'react';
-import { Layout } from 'antd';
-import 'antd/dist/antd.css';
-import './home.css'
-const { Header, Sider, Content } = Layout;
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Layout, Menu, Icon } from 'antd';
+import "./home.css";
 
-class home extends Component {
-    render() {
-      return (
-        <Layout className = "layout">
-          <Sider >
-            <Header><h1 color = 'white'>Categorias</h1></Header>
-          </Sider>
-          <Layout>
-            <Header><h1>Receitas</h1></Header>
-            <Content></Content>
-          </Layout>
-        </Layout>
-    );
-  }
+const { Header, Content, Footer, Sider } = Layout;
+
+export default function Home({ links, reflash, children }, ...props) {
+
+  reflash();
+
+  useEffect(() => {
+    console.log(window.location.pathname);
+  }, [props]);
+
+  return (
+    <Layout className="body">
+    <Sider
+      breakpoint="lg"
+      collapsedWidth="0"
+      onBreakpoint={broken => {
+        console.log(broken);
+      }}
+      onCollapse={(collapsed, type) => {
+        console.log(collapsed, type);
+      }}
+    >
+      <div className="logo" />
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={[(window.location.pathname === '/') ? 'home' : window.location.pathname]}>
+        <Menu.Item key="home">
+          <Link to="/">
+            <Icon type="home" />
+            <span className="nav-text">Todas Categorias</span>
+          </Link>
+        </Menu.Item>
+
+        {
+          links.map((link, index) => (
+              <Menu.Item key={ link.path }>
+                 <Link to={link.path}>
+                  <Icon type="apple" />
+                  <span className="nav-text">{ link.name}</span>
+                </Link>
+              </Menu.Item>
+            )
+          )
+        }
+      </Menu>
+    </Sider>
+    <Layout>
+      <Header style={{ background: '#fff', padding: 0 }} />
+      <Content style={{ margin: '24px 16px' }}>
+        <div className="innerBody" style={{ padding: 24, background: '#fff' }}>
+          {children}
+        </div>
+      </Content>
+    </Layout>
+  </Layout>
+  );
 }
-
-  export default home;
